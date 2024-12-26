3df2620e21ce8449ecaed03040efd1a5fa50f4ee
novikoviam <novikoviam@gmail.com>
2024-12-26 18:59:28 +0300

    - Gradle updated - PracticeList logic realized

--- src/main/java/com/novikoviam/PracticeList.java
+++ src/main/java/com/novikoviam/PracticeList.java
@@ -1,4 +1,34 @@
 package com.novikoviam;
 
+import java.util.Vector;
+
 public class PracticeList {
-}
+    private Vector<String> values;
+    PracticeList(String[] arrayNames) {
+        values = new Vector<>();
+        for (int i = 0; i < arrayNames.length; i++) {
+            values.add(arrayNames[i]);
+        }
+    }
+
+    public void addNewName(String newName){
+        values.add(newName);
+    }
+
+    public boolean search(String searchedName){
+        return values.contains(searchedName);
+    }
+
+    public void deleteOldName(int index){
+        Vector<String> newValues = new Vector<String>();
+        for (int i = 0; i < values.size(); i++) if (i != index) newValues.add(values.get(i));
+        values = newValues;
+    }
+
+    @Override
+    public String toString(){
+        return values.toString();
+    }
+
+    }
+
